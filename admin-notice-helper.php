968d794da9d0af3a82b9e48d8d12aec21d52a16e
Ian Dunn <ian@iandunn.name>
2014-03-29 14:36:11 -0700

    Remove cruft

--- admin-notice-helper.php
+++ admin-notice-helper.php
@@ -25,19 +25,12 @@ if ( ! class_exists( 'Admin_Notice_Helper' ) ) {
 	class Admin_Notice_Helper {
 		// Declare variables and constants
 		private static $instance;
-		private $notices, $updatedNotices, $userNoticeCount, $accessiblePrivateVars, $debugMode;
-		const NAME    = 'Admin_Notice_Helper';
-		const VERSION = '0.1.3';
-		const PREFIX  = 'idan_';
+		private $notices, $updatedNotices;
 
 		/**
 		 * Constructor
-		 *
-		 * @mvc    Controller
-		 * @author Ian Dunn <ian@iandunn.name>
 		 */
 		private function __construct() {
-			// NOTE: Make sure you update the did_action() parameter in the corresponding callback method when changing the hooks here
 			add_action( 'init',          array( $this, 'init' ), 9 );         // needs to run before other plugin's init callbacks so that they can enqueue messages in their init callbacks
 			add_action( 'admin_notices', array( $this, 'printMessages' ) );
 			add_action( 'shutdown',      array( $this, 'shutdown' ) );
@@ -52,8 +45,7 @@ if ( ! class_exists( 'Admin_Notice_Helper' ) ) {
 		 */
 		public static function getSingleton() {
 			if ( ! isset( self::$instance ) ) {
-				$class          = __CLASS__;
-				self::$instance = new $class;
+				self::$instance = new Admin_Notice_Helper();
 			}
 
 			return self::$instance;
@@ -61,84 +53,37 @@ if ( ! class_exists( 'Admin_Notice_Helper' ) ) {
 
 		/**
 		 * Initializes variables
-		 *
-		 * @mvc    Controller
-		 * @author Ian Dunn <ian@iandunn.name>
 		 */
 		public function init() {
-			if ( did_action( 'init' ) !== 1 )
-				return;
-
 			$defaultNotices              = array( 'updates' => array(), 'errors' => array() );
-			$this->notices               = array_merge( $defaultNotices, get_option( self::PREFIX . 'notices', array() ) );
-			$this->userNoticeCount       = array( 'updates' => count( $this->notices['updates'] ), 'errors' => count( $this->notices['errors'] ) ); // @todo - don't you need to check if the messages are 'user' mode or not?
+			$this->notices               = array_merge( $defaultNotices, get_option( 'anh_notices', array() ) );
+			$this->userNoticeCount       = array( 'updates' => count( $this->notices['updates'] ), 'errors' => count( $this->notices['errors'] ) );
 			$this->updatedNotices        = false;
-			$this->accessiblePrivateVars = array( 'debugMode' );
-			$this->debugMode             = false;
-		}
-
-		/**
-		 * Public getter for private variables
-		 *
-		 * @mvc    Model
-		 * @author Ian Dunn <ian@iandunn.name>
-		 * @param string $variable
-		 * @return mixed
-		 */
-		public function __get( $variable ) {
-			if ( in_array( $variable, $this->accessiblePrivateVars ) ) {
-				return $this->$variable;
-			} else {
-				throw new Exception( self::NAME . " error: Variable '" . $variable . "' doesn't exist or isn't accessible." );
-			}
-		}
-
-		/**
-		 * Public setter for private variables
-		 *
-		 * @mvc    Model
-		 * @author Ian Dunn <ian@iandunn.name>
-		 * @param string $variable
-		 * @param mixed  $value
-		 */
-		public function __set( $variable, $value ) {
-			if ( in_array( $variable, $this->accessiblePrivateVars ) ) {
-				$this->$variable = $value;
-			} else {
-				throw new Exception( self::NAME . " error: Variable '" . $variable . "' doesn't exist or isn't accessible." );
-			}
 		}
 
 		/**
 		 * Queues up a message to be displayed to the user
+		 *
 		 * NOTE: In order to allow HTML in the output, any unsafe variables in $message need to be escaped before they're passed in, instead of escaping here.
 		 *
-		 * @mvc    Model
-		 * @author Ian Dunn <ian@iandunn.name>
 		 * @param string $message The text to show the user
 		 * @param string $type    'update' for a success or notification message, or 'error' for an error message
-		 * @param string $mode    'user' if it's intended for the user, or 'debug' if it's intended for the developer
 		 */
-		public function enqueue( $message, $type = 'update', $mode = 'user' ) {
-			$message = apply_filters( self::PREFIX . 'enqueue-message', $message );
+		public function enqueue( $message, $type = 'update' ) {
+			$message = apply_filters( 'anh_enqueue-message', $message );
 
 			if ( ! is_string( $message ) ) {
 				return false;
 			}
 
-			if ( ! isset( $this->notices[$type . 's'] ) ) {
+			if ( ! isset( $this->notices[ $type . 's' ] ) ) {
 				return false;
 			}
 
-			array_push( $this->notices[$type . 's'], array(
+			array_push( $this->notices[ $type . 's' ], array(
 				'message' => $message,
 				'type'    => $type,
-				'mode'    => $mode
 			) );
-
-			if ( $mode == 'user' ) {
-				$this->userNoticeCount[$type . 's'] ++;
-			}
 
 			$this->updatedNotices = true;
 
@@ -147,41 +92,28 @@ if ( ! class_exists( 'Admin_Notice_Helper' ) ) {
 
 		/**
 		 * Displays updates and errors
-		 *
-		 * @mvc    Model
-		 * @author Ian Dunn <ian@iandunn.name>
 		 */
 		public function printMessages() {
-			if ( did_action( 'admin_notices' ) !== 1 ) {
-				return;
-			}
-
 			foreach ( array( 'updates', 'errors' ) as $type ) {
-				if ( $this->notices[$type] && ( $this->debugMode || $this->userNoticeCount[$type] ) ) {
+				if ( $this->notices[ $type ] && $this->userNoticeCount[ $type ] ) {
 					$message = '';
 					$class   = $type == 'updates' ? 'updated' : 'error';
 
-					require( dirname( __FILE__ ) . '/v-admin-notice.php' );
+					require( dirname( __FILE__ ) . '/admin-notice.php' );
 
-					$this->notices[$type]         = array();
-					$this->updatedNotices         = true;
-					$this->userNoticeCount[$type] = 0;
+					$this->notices[ $type ]         = array();
+					$this->updatedNotices           = true;
+					$this->userNoticeCount[ $type ] = 0;
 				}
 			}
 		}
 
 		/**
 		 * Writes notices to the database
-		 *
-		 * @mvc    Controller
-		 * @author Ian Dunn <ian@iandunn.name>
 		 */
 		public function shutdown() {
-			if ( did_action( 'shutdown' ) !== 1 )
-				return;
-
 			if ( $this->updatedNotices ) {
-				update_option( self::PREFIX . 'notices', $this->notices );
+				update_option( 'anh_notices', $this->notices );
 			}
 		}
 	} // end Admin_Notice_Helper
--- admin-notice.php
+++ admin-notice.php
@@ -1,4 +1,4 @@
-<div class="<?php echo self::PREFIX; ?>message <?php esc_attr_e( $class ); ?>">
+<div class="anh_message <?php esc_attr_e( $class ); ?>">
 	<?php foreach ( $this->notices[$type] as $messageData ) : ?>
 		<?php if ( $messageData['mode'] == 'user' || $this->debugMode ) : ?>
 			<p><?php esc_html_e( $messageData['message'] ); ?></p>
