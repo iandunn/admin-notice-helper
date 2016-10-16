83a147c893f741595bfac9ca62124afc2464308d
Guido <dev@guidoscialfa.com>
2016-10-16 11:37:16 +0200

    Sanitize class value
    Use sanitize_html_class over esc_attr.

--- admin-notice.php
+++ admin-notice.php
@@ -1,4 +1,4 @@
-<div class="anh_message <?php esc_attr_e( $class ); ?>">
+<div class="anh_message <?php echo sanitize_html_class( $class ); ?>">
 	<?php foreach ( $this->notices[ $type ] as $notice ) : ?>
 		<p><?php echo wp_kses( $notice, wp_kses_allowed_html( 'post' ) ); ?></p>
 	<?php endforeach; ?>
